ab6daaf437572774ee80f9e923a99f65b9ca0294
jie <tsy31415926@163.com>
2018-01-22 01:19:28 +0800

    sort the file

--- first_day/first.py
+++
@@ -1,5 +0,0 @@
-print('hello,world')
-print('Python is interesting')
-print('I will study python')
-print('Git is a free software')
-print('I have a dream')
--- third_day/sockt_try/server.py
+++
@@ -1,3 +0,0 @@
-import socket
-s = socket.socket()
-s.bind()
